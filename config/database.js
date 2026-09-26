ec79cf41c21c6bf14db5b690db066bfa0df43aa5
Alejandro <element3999@gmail.com>
2026-09-26 06:34:24 +0200

    segunda actualización backend 26-09-2026

--- config/database.js
+++ config/database.js
@@ -1,7 +1,8 @@
 const sqlite3 = require('sqlite3').verbose();
 const path = require('path');
 
-const dbPath = path.resolve(__dirname, '../../database/schema.db');
+//const dbPath = path.resolve(__dirname, '../../database/schema.db'); para local
+const dbPath = path.resolve(__dirname, '../database/schema.db'); //para render
 
 const db = new sqlite3.Database(dbPath, (err) => {
 
